47ddc4c5089a3ea36d32b53de3120b9a85e1d2aa
Benoit Blanchon <github@benoitblanchon.fr>
2018-06-05 10:33:15 +0200

    Added msgpack fuzzer

--- fuzzing/fuzz.sh
+++ fuzzing/fuzz.sh
@@ -3,7 +3,8 @@
 
 cd $(dirname $0)
 export CXX='clang++'
-export CXXFLAGS='-fsanitize-coverage=trace-pc-guard -fsanitize=address,fuzzer'
+export CXXFLAGS='-fsanitize=address,fuzzer'
 export LIB_FUZZING_ENGINE=
 make OUT=.
-./json_fuzzer my_json_corpus json_corpus -max_len=1024 -timeout=10
+# ./json_fuzzer json_corpus json_seed_corpus -max_len=1024 -max_total_time=10
+./msgpack_fuzzer msgpack_corpus msgpack_seed_corpus -max_len=1024
